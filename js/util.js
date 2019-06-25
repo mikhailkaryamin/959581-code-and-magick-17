41e909540900f2e7166e7fa25b6e264e8b0d6c35
mikhailkaryamin <karyamin.mihail@yandex.ru>
2019-06-25 19:06:04 +0500

    модуляция

---
+++ js/util.js
@@ -0,0 +1,22 @@
+'use strict';
+
+window.util = (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  return {
+    isEscEvent: function (evt, action) {
+      var inputNameElement = document.querySelector('.setup-user-name:focus');
+
+      if (evt.keyCode === ESC_KEYCODE && !inputNameElement) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    }
+  };
+})();
+
